985372fb0066aafc5008ddd475a2514d89f7a547
reintrance <reintrance@gmail.com>
2017-06-19 16:40:20 +0300

    Draft of ProductList component

---
+++ voting_app/app.js
@@ -0,0 +1,9 @@
+const ProductList = React.createClass({
+    render: function () {
+        return (
+            <div className='ui items'>
+                Hey there!
+            </div>
+        );
+    }
+});
