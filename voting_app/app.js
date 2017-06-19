73876a092e645b2fab4eebde081a6a27b8a224fd
reintrance <reintrance@gmail.com>
2017-06-19 23:54:24 +0300

    Refactor component definition to ES6 style

--- voting_app/app.js
+++ voting_app/app.js
@@ -1,11 +1,15 @@
-const ProductList = React.createClass({
-    render: function () {
+class ProductList extends React.Component {
+    constructor (props) {
+        super(props)
+    }
+
+    render () {
         return (
             <div className='ui items'>
                 Hey there!
             </div>
         );
     }
-});
+};
 
 ReactDOM.render(<ProductList />, document.getElementById('content'));
