44253d3c6c37594d5978a7253a6433a0e0a9279d
reintrance <reintrance@gmail.com>
2017-06-20 12:57:30 +0300

    Created mock data

---
+++ voting_app/data.js
@@ -0,0 +1,82 @@
+const data = [
+  {
+    "id": 0,
+    "title": "dolore",
+    "description": "Dolore id aliqua consequat excepteur.",
+    "votes": 8,
+    "submitter_avatar_url": "http://placehold.it/32x32",
+    "picture_url": "http://placehold.it/150x150"
+  },
+  {
+    "id": 1,
+    "title": "cillum",
+    "description": "Proident id anim eiusmod nisi dolor proident officia laboris Lorem duis adipisicing sint ex.",
+    "votes": 5,
+    "submitter_avatar_url": "http://placehold.it/32x32",
+    "picture_url": "http://placehold.it/150x150"
+  },
+  {
+    "id": 2,
+    "title": "esse",
+    "description": "Qui culpa aute excepteur enim cupidatat do do ea excepteur.",
+    "votes": 15,
+    "submitter_avatar_url": "http://placehold.it/32x32",
+    "picture_url": "http://placehold.it/150x150"
+  },
+  {
+    "id": 3,
+    "title": "id",
+    "description": "Cillum elit enim fugiat nulla nisi sit incididunt et.",
+    "votes": 39,
+    "submitter_avatar_url": "http://placehold.it/32x32",
+    "picture_url": "http://placehold.it/150x150"
+  },
+  {
+    "id": 4,
+    "title": "officia",
+    "description": "Laboris tempor proident non incididunt labore ullamco eu.",
+    "votes": 1,
+    "submitter_avatar_url": "http://placehold.it/32x32",
+    "picture_url": "http://placehold.it/150x150"
+  },
+  {
+    "id": 5,
+    "title": "aute",
+    "description": "Ullamco est sit sit magna exercitation non sit eu minim.",
+    "votes": 6,
+    "submitter_avatar_url": "http://placehold.it/32x32",
+    "picture_url": "http://placehold.it/150x150"
+  },
+  {
+    "id": 6,
+    "title": "nulla",
+    "description": "Tempor labore officia dolor esse id ullamco.",
+    "votes": 35,
+    "submitter_avatar_url": "http://placehold.it/32x32",
+    "picture_url": "http://placehold.it/150x150"
+  },
+  {
+    "id": 7,
+    "title": "anim",
+    "description": "Amet sunt proident fugiat do ut velit eu pariatur proident amet non qui anim deserunt.",
+    "votes": 36,
+    "submitter_avatar_url": "http://placehold.it/32x32",
+    "picture_url": "http://placehold.it/150x150"
+  },
+  {
+    "id": 8,
+    "title": "aliquip",
+    "description": "Enim exercitation reprehenderit anim Lorem anim eiusmod nostrud ipsum esse tempor laborum ut Lorem deserunt.",
+    "votes": 95,
+    "submitter_avatar_url": "http://placehold.it/32x32",
+    "picture_url": "http://placehold.it/150x150"
+  },
+  {
+    "id": 9,
+    "title": "voluptate",
+    "description": "Dolore voluptate adipisicing commodo voluptate ipsum eiusmod quis sint ad proident amet.",
+    "votes": 22,
+    "submitter_avatar_url": "http://placehold.it/32x32",
+    "picture_url": "http://placehold.it/150x150"
+  }
+];
