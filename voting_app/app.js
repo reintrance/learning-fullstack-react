a1fcc500cc3819052766fc68d0e24eb88151f294
reintrance <reintrance@gmail.com>
2017-06-20 12:56:34 +0300

    Updated Product component template

--- voting_app/app.js
+++ voting_app/app.js
@@ -9,6 +9,17 @@ class Product extends React.Component {
                 <div className="image">
                     <img src="https://facebook.github.io/react/img/logo.svg" />
                 </div>
+                <div className="middle aligned content">
+                    <div className="description">
+                        <a>ReactJS</a>
+                        <p>Popular JS framework from Facebook</p>
+                    </div>
+                    <div className="extra">
+                        <span>Submitted by:</span>
+                        <img className="ui avatar image"
+                            src="http://findicons.com/files/icons/1072/face_avatars/300/i05.png"/>
+                    </div>
+                </div>
             </div>
         );
     }
