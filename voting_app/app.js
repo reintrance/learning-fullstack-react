28a92dbc7797f193e7f7359ca1eb72dd2e39360e
reintrance <reintrance@gmail.com>
2017-06-20 17:04:14 +0300

    Draft. Upvoting Product

--- voting_app/app.js
+++ voting_app/app.js
@@ -1,28 +1,34 @@
 class Product extends React.Component {
     constructor (props) {
         super(props);
+
+        this.handleUpVote = this.handleUpVote.bind(this);
+    }
+
+    handleUpVote () {
+        this.props.onVote(this.props.id);
     }
 
     render () {
         return (
-            <div className="item">
-                <div className="image">
+            <div className='item'>
+                <div className='image'>
                     <img src={this.props.product_image_url} />
                 </div>
-                <div className="middle aligned content">
-                    <div className="header">
-                        <a>
-                            <i className="large caret up icon"></i>
+                <div className='middle aligned content'>
+                    <div className='header'>
+                        <a onClick={this.handleUpVote}>
+                            <i className='large caret up icon'></i>
                         </a>
                         {this.props.votes}
                     </div>
-                    <div className="description">
-                        <a>{this.props.title}</a>
+                    <div className='description'>
+                        <a href='this.prop.url'>{this.props.title}</a>
                         <p>{this.props.description}</p>
                     </div>
-                    <div className="extra">
+                    <div className='extra'>
                         <span>Submitted by:</span>
-                        <img className="ui avatar image"
+                        <img className='ui avatar image'
                             src={this.props.submitter_avatar_url}/>
                     </div>
                 </div>
@@ -36,11 +42,15 @@ class ProductList extends React.Component {
         super(props)
     }
 
+    handleProductUpVote (id) {
+        console.log(`Product with id ${id} just got upvote`);
+    }
+
     render () {
-        const product = MockData[0];
-        return (
-            <div className='ui items'>
+        const products = MockData.map(product => {
+            return (
                 <Product
+                    key={'product-' + product.id}
                     id={product.id}
                     title={product.title}
                     description={product.description}
@@ -48,7 +58,13 @@ class ProductList extends React.Component {
                     url={product.url}
                     submitter_avatar_url={product.submitter_avatar_url}
                     product_image_url={product.product_image_url}
+                    onVote={this.handleProductUpVote}
                 />
+            );
+        });
+        return (
+            <div className='ui items'>
+                {products}
             </div>
         );
     }
