fda2e90b2f5f7060e6e4bad5c106625f9be16aff
reintrance <reintrance@gmail.com>
2017-06-20 16:12:32 +0300

    Updated MockData

--- voting_app/data.js
+++ voting_app/data.js
@@ -1,82 +1,46 @@
-const MockData = [
-  {
-    "id": 0,
-    "title": "dolore",
-    "description": "Dolore id aliqua consequat excepteur.",
-    "votes": 8,
-    "submitter_avatar_url": "http://placehold.it/32x32",
-    "picture_url": "http://placehold.it/150x150"
-  },
-  {
-    "id": 1,
-    "title": "cillum",
-    "description": "Proident id anim eiusmod nisi dolor proident officia laboris Lorem duis adipisicing sint ex.",
-    "votes": 5,
-    "submitter_avatar_url": "http://placehold.it/32x32",
-    "picture_url": "http://placehold.it/150x150"
-  },
-  {
-    "id": 2,
-    "title": "esse",
-    "description": "Qui culpa aute excepteur enim cupidatat do do ea excepteur.",
-    "votes": 15,
-    "submitter_avatar_url": "http://placehold.it/32x32",
-    "picture_url": "http://placehold.it/150x150"
-  },
-  {
-    "id": 3,
-    "title": "id",
-    "description": "Cillum elit enim fugiat nulla nisi sit incididunt et.",
-    "votes": 39,
-    "submitter_avatar_url": "http://placehold.it/32x32",
-    "picture_url": "http://placehold.it/150x150"
-  },
-  {
-    "id": 4,
-    "title": "officia",
-    "description": "Laboris tempor proident non incididunt labore ullamco eu.",
-    "votes": 1,
-    "submitter_avatar_url": "http://placehold.it/32x32",
-    "picture_url": "http://placehold.it/150x150"
-  },
-  {
-    "id": 5,
-    "title": "aute",
-    "description": "Ullamco est sit sit magna exercitation non sit eu minim.",
-    "votes": 6,
-    "submitter_avatar_url": "http://placehold.it/32x32",
-    "picture_url": "http://placehold.it/150x150"
-  },
-  {
-    "id": 6,
-    "title": "nulla",
-    "description": "Tempor labore officia dolor esse id ullamco.",
-    "votes": 35,
-    "submitter_avatar_url": "http://placehold.it/32x32",
-    "picture_url": "http://placehold.it/150x150"
-  },
-  {
-    "id": 7,
-    "title": "anim",
-    "description": "Amet sunt proident fugiat do ut velit eu pariatur proident amet non qui anim deserunt.",
-    "votes": 36,
-    "submitter_avatar_url": "http://placehold.it/32x32",
-    "picture_url": "http://placehold.it/150x150"
-  },
-  {
-    "id": 8,
-    "title": "aliquip",
-    "description": "Enim exercitation reprehenderit anim Lorem anim eiusmod nostrud ipsum esse tempor laborum ut Lorem deserunt.",
-    "votes": 95,
-    "submitter_avatar_url": "http://placehold.it/32x32",
-    "picture_url": "http://placehold.it/150x150"
-  },
-  {
-    "id": 9,
-    "title": "voluptate",
-    "description": "Dolore voluptate adipisicing commodo voluptate ipsum eiusmod quis sint ad proident amet.",
-    "votes": 22,
-    "submitter_avatar_url": "http://placehold.it/32x32",
-    "picture_url": "http://placehold.it/150x150"
+window.MockData = (function () {
+  function generateVoteCount() {
+    return Math.floor((Math.random() * 50) + 15);
   }
-];
+
+  const data = [
+    {
+      id: 1,
+      title: 'Yellow Pail',
+      description: 'On-demand sand castle construction expertise.',
+      url: '#',
+      votes: generateVoteCount(),
+      submitter_avatar_url: 'images/avatars/daniel.jpg',
+      product_image_url: 'images/products/image-aqua.png',
+    },
+    {
+      id: 2,
+      title: 'Supermajority: The Fantasy Congress League',
+      description: 'Earn points when your favorite politicians pass legislation.',
+      url: '#',
+      votes: generateVoteCount(),
+      submitter_avatar_url: 'images/avatars/kristy.png',
+      product_image_url: 'images/products/image-rose.png',
+    },
+    {
+      id: 3,
+      title: 'Tinfoild: Tailored tinfoil hats',
+      description: 'We already have your measurements and shipping address.',
+      url: '#',
+      votes: generateVoteCount(),
+      submitter_avatar_url: 'images/avatars/veronika.jpg',
+      product_image_url: 'images/products/image-steel.png',
+    },
+    {
+      id: 4,
+      title: 'Haught or Naught',
+      description: 'High-minded or absent-minded? You decide.',
+      url: '#',
+      votes: generateVoteCount(),
+      submitter_avatar_url: 'images/avatars/molly.png',
+      product_image_url: 'images/products/image-yellow.png',
+    },
+  ];
+
+  return data;
+})();
